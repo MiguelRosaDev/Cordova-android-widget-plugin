970cfc9d81288a17fb169115e1653790764e3d3e
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2024-11-07 14:18:38 +0000

    Update WidgetPlugin.java

--- src/android/WidgetPlugin.java
+++ src/android/WidgetPlugin.java
@@ -8,8 +8,11 @@ import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class WidgetPlugin extends CordovaPlugin {
+    private static final String TAG = "WidgetPlugin";
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("updateWidget")) {
@@ -21,15 +24,21 @@ public class WidgetPlugin extends CordovaPlugin {
     }
 
     private void updateWidget(String text, CallbackContext callbackContext) {
-        Context context = this.cordova.getActivity().getApplicationContext();
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-        ComponentName thisWidget = new ComponentName(context, WidgetProvider.class);
-        Intent intent = new Intent(context, WidgetProvider.class);
-        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-        intent.putExtra("widgetText", text);
-        int[] ids = appWidgetManager.getAppWidgetIds(thisWidget);
-        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
-        context.sendBroadcast(intent);
-        callbackContext.success("Widget updated successfully");
+        try {
+            Context context = this.cordova.getActivity().getApplicationContext();
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            ComponentName thisWidget = new ComponentName(context, WidgetProvider.class);
+            Intent intent = new Intent(context, WidgetProvider.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra("widgetText", text);
+            int[] ids = appWidgetManager.getAppWidgetIds(thisWidget);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
+            context.sendBroadcast(intent);
+            Log.d(TAG, "Update widget intent sent with text: " + text);
+            callbackContext.success("Widget update request sent successfully");
+        } catch (Exception e) {
+            Log.e(TAG, "Error updating widget: " + e.getMessage());
+            callbackContext.error("Failed to update widget: " + e.getMessage());
+        }
     }
 }
