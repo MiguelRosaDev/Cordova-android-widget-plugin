523cb4a8368753fb8fa1e731c2476c01c46314e1
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2024-11-11 18:00:48 +0000

    Update widget.js

--- www/widget.js
+++ www/widget.js
@@ -3,6 +3,9 @@ var exec = require('cordova/exec');
 var AndroidWidget = {
     updateWidget: function(text, successCallback, errorCallback) {
         exec(successCallback, errorCallback, "AndroidWidget", "updateWidget", [text]);
+    },
+    listenForButtonClicks: function(successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "AndroidWidget", "listenForButtonClicks", []);
     }
 };
 
