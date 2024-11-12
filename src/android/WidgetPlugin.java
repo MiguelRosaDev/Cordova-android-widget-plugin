64332d362d50df8246b443174bcd76646bb4f896
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2024-11-12 16:25:45 +0000

    Update WidgetPlugin.java

--- src/android/WidgetPlugin.java
+++ src/android/WidgetPlugin.java
@@ -16,17 +16,9 @@ import android.content.IntentFilter;
 
 public class WidgetPlugin extends CordovaPlugin {
     private static final String TAG = "WidgetPlugin";
-    private static final String BUTTON_CLICKED_ACTION = "com.example.BUTTON_CLICKED";
-    
     private CallbackContext buttonClickCallbackContext;
     private BroadcastReceiver buttonClickReceiver;
-
-    @Override
-    public void initialize(org.apache.cordova.CordovaInterface cordova, org.apache.cordova.CordovaWebView webView) {
-        super.initialize(cordova, webView);
-        registerButtonClickReceiver();
-    }
-
+    
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("updateWidget")) {
@@ -64,35 +56,5 @@ public class WidgetPlugin extends CordovaPlugin {
         PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
         pluginResult.setKeepCallback(true);
         callbackContext.sendPluginResult(pluginResult);
-    }
-    
-    private void registerButtonClickReceiver() {
-        if (buttonClickReceiver == null) {
-            buttonClickReceiver = new BroadcastReceiver() {
-                @Override
-                public void onReceive(Context context, Intent intent) {
-                    if (BUTTON_CLICKED_ACTION.equals(intent.getAction())) {
-                        Log.d(TAG, "Button clicked broadcast received in WidgetPlugin");
-                        if (buttonClickCallbackContext != null) {
-                            PluginResult result = new PluginResult(PluginResult.Status.OK);
-                            result.setKeepCallback(true);
-                            buttonClickCallbackContext.sendPluginResult(result);
-                        }
-                    }
-                }
-            };
-            IntentFilter filter = new IntentFilter(BUTTON_CLICKED_ACTION);
-            this.cordova.getActivity().registerReceiver(buttonClickReceiver, filter);
-            Log.d(TAG, "Button click BroadcastReceiver registered in WidgetPlugin");
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (buttonClickReceiver != null) {
-            this.cordova.getActivity().unregisterReceiver(buttonClickReceiver);
-            Log.d(TAG, "Button click BroadcastReceiver unregistered in WidgetPlugin");
-        }
     }
 }
