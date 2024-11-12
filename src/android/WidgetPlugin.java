6c94734810f2e5888479262421a9dfaa7d56c982
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2024-11-12 16:05:44 +0000

    Update WidgetPlugin.java

--- src/android/WidgetPlugin.java
+++ src/android/WidgetPlugin.java
@@ -17,23 +17,16 @@ import android.content.IntentFilter;
 public class WidgetPlugin extends CordovaPlugin {
     private static final String TAG = "WidgetPlugin";
     private static final String BUTTON_CLICKED_ACTION = "com.example.BUTTON_CLICKED";
-    private CallbackContext buttonClickCallback;
-
-        
-    private final BroadcastReceiver buttonClickReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (BUTTON_CLICKED_ACTION.equals(intent.getAction())) {
-                Log.d(TAG, "Button clicked broadcast received in WidgetPlugin");
-                if (buttonClickCallback != null) {
-                    PluginResult result = new PluginResult(PluginResult.Status.OK);
-                    result.setKeepCallback(true);
-                    buttonClickCallback.sendPluginResult(result);
-                }
-            }
-        }
-    };
     
+    private CallbackContext buttonClickCallbackContext;
+    private BroadcastReceiver buttonClickReceiver;
+
+    @Override
+    public void initialize(org.apache.cordova.CordovaInterface cordova, org.apache.cordova.CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        registerButtonClickReceiver();
+    }
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("updateWidget")) {
@@ -72,19 +65,34 @@ public class WidgetPlugin extends CordovaPlugin {
         pluginResult.setKeepCallback(true);
         callbackContext.sendPluginResult(pluginResult);
     }
-
-    @Override
-    public void initialize(final Context context, final Intent intent) {
-        super.initialize(context, intent);
-        IntentFilter filter = new IntentFilter(BUTTON_CLICKED_ACTION);
-        context.registerReceiver(buttonClickReceiver, filter);
-        Log.d(TAG, "BroadcastReceiver for button clicks registered in WidgetPlugin");
+    
+    private void registerButtonClickReceiver() {
+        if (buttonClickReceiver == null) {
+            buttonClickReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    if (BUTTON_CLICKED_ACTION.equals(intent.getAction())) {
+                        Log.d(TAG, "Button clicked broadcast received in WidgetPlugin");
+                        if (buttonClickCallbackContext != null) {
+                            PluginResult result = new PluginResult(PluginResult.Status.OK);
+                            result.setKeepCallback(true);
+                            buttonClickCallbackContext.sendPluginResult(result);
+                        }
+                    }
+                }
+            };
+            IntentFilter filter = new IntentFilter(BUTTON_CLICKED_ACTION);
+            this.cordova.getActivity().registerReceiver(buttonClickReceiver, filter);
+            Log.d(TAG, "Button click BroadcastReceiver registered in WidgetPlugin");
+        }
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        cordova.getActivity().unregisterReceiver(buttonClickReceiver);
-        Log.d(TAG, "BroadcastReceiver for button clicks unregistered in WidgetPlugin");
+        if (buttonClickReceiver != null) {
+            this.cordova.getActivity().unregisterReceiver(buttonClickReceiver);
+            Log.d(TAG, "Button click BroadcastReceiver unregistered in WidgetPlugin");
+        }
     }
 }
