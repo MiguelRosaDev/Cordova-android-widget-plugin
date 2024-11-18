ebfaeee0c04b8b2ebd37f1e3e6614876e95e867e
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2024-11-18 15:44:35 +0000

    Update WidgetProvider.java

--- src/android/WidgetProvider.java
+++ src/android/WidgetProvider.java
@@ -49,7 +49,7 @@ public class WidgetProvider extends AppWidgetProvider {
             } else {
                 Log.e(TAG, "WidgetPlugin instance is null");
             }
-        } else if (intent.getAction().equals(AppWidgetManager.ACTION_APP_CLOSED)) {
+        } else if (ACTION_APP_CLOSED.equals(intent.getAction())) {
             Log.d(TAG, "App closed event received in widget");
             String widgetText = intent.getStringExtra("widgetText");
             AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
